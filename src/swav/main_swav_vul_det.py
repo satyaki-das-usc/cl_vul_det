35c7ff836f2b38adc21c7cc29488682cb94d61ed
Saad Shafiq <51906406+ssshafiq@users.noreply.github.com>
2025-09-15 19:25:33 -0700

    updated imbalancedsampler for oversampling

--- src/swav/main_swav_vul_det.py
+++ src/swav/main_swav_vul_det.py
@@ -269,7 +269,7 @@ if __name__ == "__main__":
             with open(slice_path, "rb") as rbfi:
                 slice_graph: nx.DiGraph = pickle.load(rbfi)
                 ys.append(slice_graph.graph["label"])
-        sampler = ImbalancedSampler(torch.tensor(ys, dtype=torch.long))
+        sampler = ImbalancedSampler(torch.tensor(ys, dtype=torch.long), num_samples=ys.count(0)*2)
     data_module = SliceDataModule(config, vocab, train_sampler=sampler, use_temp_data=args.use_temp_data)
     logging.info("Data module loading completed.")
 
