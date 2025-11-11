c02e7c5b91de16965b99ca180c77cab35f88a781
Satyaki Das <satyakid@usc.edu>
2025-11-11 11:10:35 -0800

    change run sequence

--- run.sh
+++ run.sh
@@ -42,8 +42,8 @@ PYTHONPATH="." python src/tokenize_slices.py &&
 PYTHONPATH="." python src/generate_file_slice_mapping.py &&
 PYTHONPATH="." python src/preprocess/remove_duplicates.py &&
 xargs -d '\n' -a duplicate_slices.txt rm -- &&
-PYTHONPATH="." python src/preprocess/word_embedding.py &&
 PYTHONPATH="." python src/preprocess/split_dataset.py &&
+PYTHONPATH="." python src/preprocess/word_embedding.py &&
 PYTHONPATH="." python src/swav/main_swav_vul_det.py &&
 PYTHONPATH="." python src/preprocess/generate_instance_perturbation_mapping.py &&
 PYTHONPATH="." python src/preprocess/generate_swav_batches.py &&
