6ace7fd7699120edb3d8dbaecf8eea67aa855672
Satyaki Das <88107775+satyaki-das-usc@users.noreply.github.com>
2025-11-18 11:28:06 -0800

    updated run scripts

--- run.sh
+++ run.sh
@@ -54,6 +54,7 @@ xargs -d '\n' -a duplicate_slices.txt rm -- &&
 PYTHONPATH="." python src/preprocess/split_dataset.py &&
 PYTHONPATH="." python src/preprocess/word_embedding.py &&
 PYTHONPATH="." python src/swav/main_swav_vul_det.py &&
+PYTHONPATH="." python src/swav/main_swav_vul_det.py --skip_training &&
 PYTHONPATH="." python src/preprocess/generate_instance_perturbation_mapping.py &&
 PYTHONPATH="." python src/preprocess/generate_swav_batches.py &&
 PYTHONPATH="." python src/preprocess/generate_multicrop_swav_batches.py &&
