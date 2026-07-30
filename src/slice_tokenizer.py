a154355e470c9082075eb24924166d32dd8829a8
satyaki-das-usc <satyakid@usc.edu>
2026-07-29 17:39:25 -0700

    more module level optimization

--- src/slice_tokenizer.py
+++ src/slice_tokenizer.py
@@ -10,6 +10,14 @@ from os.path import join
 from omegaconf import DictConfig
 from transformers import RobertaTokenizer
 
+STRING_LITERAL_RE = re.compile(r'".*?"')
+CHAR_LITERAL_RE = re.compile(r"'.*?'")
+NON_ASCII_RE = re.compile(r'[^\x00-\x7f]')
+FUNCTION_RE = re.compile(r'\b([_A-Za-z]\w*)\b(?=\s*\()')
+VARIABLE_RE = re.compile(
+    r'\b([_A-Za-z]\w*)\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()'
+)
+
 @lru_cache(maxsize=None)
 def load_tokenizer_resources(sensi_api_path: str):
     operators3 = frozenset({'<<=', '>>='})
@@ -286,13 +294,6 @@ class SliceTokenizer:
 
         # regular expression to catch multi-line comment
         # rx_comment = re.compile('\*/\s*$')
-        # regular expression to find function name candidates
-        rx_fun = re.compile(r'\b([_A-Za-z]\w*)\b(?=\s*\()')
-        # regular expression to find variable name candidates
-        # rx_var = re.compile(r'\b([_A-Za-z]\w*)\b(?!\s*\()')
-        rx_var = re.compile(
-            r'\b([_A-Za-z]\w*)\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()')
-
         # final cleaned gadget output to return to interface
         cleaned_gadget = []
 
@@ -300,15 +301,15 @@ class SliceTokenizer:
             # process if not the header line and not a multi-line commented line
             # if rx_comment.search(line) is None:
             # remove all string literals (keep the quotes)
-            nostrlit_line = re.sub(r'".*?"', '""', line)
+            nostrlit_line = STRING_LITERAL_RE.sub('""', line)
             # remove all character literals
-            nocharlit_line = re.sub(r"'.*?'", "''", nostrlit_line)
+            nocharlit_line = CHAR_LITERAL_RE.sub("''", nostrlit_line)
             # replace any non-ASCII characters with empty string
-            ascii_line = re.sub(r'[^\x00-\x7f]', r'', nocharlit_line)
+            ascii_line = NON_ASCII_RE.sub('', nocharlit_line)
 
             # return, in order, all regex matches at string list; preserves order for semantics
-            user_fun = rx_fun.findall(ascii_line)
-            user_var = rx_var.findall(ascii_line)
+            user_fun = FUNCTION_RE.findall(ascii_line)
+            user_var = VARIABLE_RE.findall(ascii_line)
 
             # Could easily make a "clean gadget" type class to prevent duplicate functionality
             # of creating/comparing symbol names for functions and variables in much the same way.
@@ -316,7 +317,7 @@ class SliceTokenizer:
             # So would only need to pass a string list and a string literal for symbol names to
             # another function.
             for fun_name in user_fun:
-                if len({fun_name}.difference(self.main_set)) != 0 and len({fun_name}.difference(self.keywords)) != 0:
+                if fun_name not in self.main_set and fun_name not in self.keywords:
                     # DEBUG
                     # print('comparing ' + str(fun_name + ' to ' + str(main_set)))
                     # print(fun_name + ' diff len from main is ' + str(len({fun_name}.difference(main_set))))
@@ -324,7 +325,7 @@ class SliceTokenizer:
                     # print(fun_name + ' diff len from keywords is ' + str(len({fun_name}.difference(keywords))))
                     ###
                     # check to see if function name already in dictionary
-                    if fun_name not in fun_symbols.keys():
+                    if fun_name not in fun_symbols:
                         fun_symbols[fun_name] = 'FUN' + str(fun_count)
                         fun_count += 1
                     # ensure that only function name gets replaced (no variable name with same
@@ -334,7 +335,7 @@ class SliceTokenizer:
 
             for var_name in user_var:
                 # next line is the nuanced difference between fun_name and var_name
-                if len({var_name}.difference(self.keywords)) != 0 and len({var_name}.difference(self.main_args)) != 0:
+                if var_name not in self.keywords and var_name not in self.main_args:
                     # DEBUG
                     # print('comparing ' + str(var_name + ' to ' + str(keywords)))
                     # print(var_name + ' diff len from keywords is ' + str(len({var_name}.difference(keywords))))
@@ -342,7 +343,7 @@ class SliceTokenizer:
                     # print(var_name + ' diff len from main args is ' + str(len({var_name}.difference(main_args))))
                     ###
                     # check to see if variable name already in dictionary
-                    if var_name not in var_symbols.keys():
+                    if var_name not in var_symbols:
                         var_symbols[var_name] = 'VAR' + str(var_count)
                         var_count += 1
                     # ensure that only variable name gets replaced (no function name with same
